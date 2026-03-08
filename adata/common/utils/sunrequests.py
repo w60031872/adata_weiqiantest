6725e3be2de8c1ce79b1c1fc8c64ca492b3d41cf
w60031872 <164104287+w60031872@users.noreply.github.com>
2026-03-08 19:42:57 +0800

    Update sunrequests.py
    q

--- adata/common/utils/sunrequests.py
+++ adata/common/utils/sunrequests.py
@@ -10,6 +10,7 @@
 
 import threading
 import time
+from urllib.parse import urlparse
 
 import requests
 
@@ -41,10 +42,70 @@ class SunProxy(object):
             del cls._data[key]
 
 
+class RateLimiter(object):
+    _instance = None
+    _instance_lock = threading.Lock()
+    _default_limit = 30
+    _limits = {}
+    _request_history = {}
+    _lock = threading.Lock()
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            with cls._instance_lock:
+                if not cls._instance:
+                    cls._instance = object.__new__(cls)
+        return cls._instance
+
+    @classmethod
+    def set_default_limit(cls, limit):
+        cls._default_limit = limit
+
+    @classmethod
+    def set_limit(cls, domain, limit):
+        cls._limits[domain] = limit
+
+    @classmethod
+    def get_limit(cls, domain):
+        return cls._limits.get(domain, cls._default_limit)
+
+    @classmethod
+    def _clean_old_requests(cls, domain, current_time):
+        if domain not in cls._request_history:
+            return
+        cls._request_history[domain] = [
+            t for t in cls._request_history[domain]
+            if current_time - t < 60
+        ]
+
+    @classmethod
+    def acquire(cls, url):
+        domain = urlparse(url).netloc
+        current_time = time.time()
+        
+        with cls._lock:
+            cls._clean_old_requests(domain, current_time)
+            
+            if domain not in cls._request_history:
+                cls._request_history[domain] = []
+            
+            limit = cls.get_limit(domain)
+            if len(cls._request_history[domain]) >= limit:
+                oldest_request = cls._request_history[domain][0]
+                wait_time = 60 - (current_time - oldest_request)
+                if wait_time > 0:
+                    time.sleep(wait_time)
+                cls._clean_old_requests(domain, time.time())
+            
+            cls._request_history[domain].append(time.time())
+            return True
+
+
 class SunRequests(object):
     def __init__(self, sun_proxy: SunProxy = None) -> None:
         super().__init__()
         self.sun_proxy = sun_proxy
+        self.rate_limiter = RateLimiter()
 
     def request(self, method='get', url=None, times=3, retry_wait_time=1588, proxies=None, wait_time=None, **kwargs):
         """
@@ -58,9 +119,11 @@ class SunRequests(object):
         :param kwargs: 其它 requests 参数，用法相同
         :return: res
         """
-        # 1. 获取设置代理
+        # 1. 频率限制
+        self.rate_limiter.acquire(url)
+        # 2. 获取设置代理
         proxies = self.__get_proxies(proxies)
-        # 2. 请求数据结果
+        # 3. 请求数据结果
         res = None
         for i in range(times):
             if wait_time:
@@ -88,6 +151,14 @@ class SunRequests(object):
         if is_proxy and ip:
             proxies = {'https': f"http://{ip}", 'http': f"http://{ip}"}
         return proxies
+
+    @classmethod
+    def set_rate_limit(cls, domain, limit):
+        RateLimiter.set_limit(domain, limit)
+
+    @classmethod
+    def set_default_rate_limit(cls, limit):
+        RateLimiter.set_default_limit(limit)
 
 
 sun_requests = SunRequests()
